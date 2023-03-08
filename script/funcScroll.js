7b919f0d5788f504ea5ff2730aed6f601ae7faa2
6Bello <giualbamonte06@gmail.com>
2023-03-08 12:31:34 +0100

    8/3 12:31

--- script/funcScroll.js
+++ script/funcScroll.js
@@ -14,8 +14,10 @@ function funcScroll() {
   }
   var slogan__gradient = document.getElementById("slogan__gradient");
   var slogan__text = document.getElementById("slogan");
-  var slogan__gradientTop = -slogan__text.getBoundingClientRect().top - 540;
+  var slogan__gradientTop = (-slogan__text.getBoundingClientRect().top - 540) / (1400 / window.innerWidth);
   var nav = document.getElementById("nav");
+  var circle = document.getElementById("nav__circle");
+  var name = document.getElementById("nav__circle__logo__name");
   slogan__gradient.style.transform = "matrix(1, 0, 0, 1, 0," + slogan__gradientTop + ")";
   if (slogan__gradientTop > -1200) {
     if (slogan__gradientTop > -571) {
@@ -25,6 +27,8 @@ function funcScroll() {
     nav.classList.add("slideInDown");
     nav.classList.remove("slideOutUp");
     nav.style.display = "block";
+    circle.style.backgroundColor = "#eeeeee00";
+    name.style.width = "10.3vw";
     $(".animapulse").addClass('animated pulse');
     $(".animapulse").removeClass('animapulse');
   } else {
@@ -33,6 +37,8 @@ function funcScroll() {
     nav.classList.add("slideOutUp");
     $(".pulse").addClass('animapulse');
     $(".animapulse").removeClass('animated pulse');
+    circle.style.backgroundColor = "#fff";
+    name.style.width = "";
   }
 
 }
