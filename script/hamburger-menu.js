a78ffcde2291d22ad3a8faa87555d4370b41cc8b
6Bello <giualbamonte06@gmail.com>
2023-03-17 17:06:10 +0100

    better-banner, better-ham-menu with animation

--- script/hamburger-menu.js
+++ script/hamburger-menu.js
@@ -1,7 +1,21 @@
 //funzione che viene usata su smartphone dopo che l'utente preme sull'icon bar per aprire il menu a tendina
 $(document).ready(function () { //serve a dire dopo che viene caricato tutto
+  let ani = 0;
   $(".hamburger-menu").click(function (e) {
-    $(".nav-list").toggleClass('is-open')
+    const hamburger = document.getElementsByClassName("nav-list");
+    console.log(hamburger[0].class);
+    if (!ani) {
+      hamburger[0].classList.add('is-open');
+      hamburger[0].classList.add('animated');
+      hamburger[0].classList.add('fadeInLeft');
+      hamburger[0].classList.remove('fadeOutLeft');
+      ani = 1;
+    } else {
+      hamburger[0].classList.remove('animated');
+      hamburger[0].classList.remove('fadeInLeft'); hamburger[0].classList.add('animated'); hamburger[0].classList.add('FadeOutLeft');
+      hamburger[0].classList.add('is-open');
+      ani = 0;
+    }
     e.preventDefault();//permette di non far ricaricare la pagine dopo il click
   })
 })
