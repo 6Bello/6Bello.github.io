e07012df4c67e06fc23766f5ea69ba212ccbe3dd
6Bello <giualbamonte06@gmail.com>
2023-03-20 14:47:00 +0100

    index-finish

--- script/script.js
+++ script/script.js
@@ -1,16 +1,16 @@
 const track = document.getElementById("image-track");
 
-const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+const handleOnDown = e => track.dataset.touchDownAt = e.clientX;
 
 const handleOnUp = () => {
-  track.dataset.mouseDownAt = "0";
+  track.dataset.touchDownAt = "0";
   track.dataset.prevPercentage = track.dataset.percentage;
 }
 
 const handleOnMove = e => {
-  if (track.dataset.mouseDownAt === "0") return;
+  if (track.dataset.touchDownAt === "0") return;
 
-  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+  const mouseDelta = parseFloat(track.dataset.touchDownAt) - e.clientX,
     maxDelta = window.innerWidth / 2;
 
   const percentage = (mouseDelta / maxDelta) * -75,
